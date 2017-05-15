58361ac7ca1a5b13344c642b9ddfc0bcb4ac2346
metehan <mthnzbk@gmail.com>
2017-05-15 19:58:42 +0300

    setup dosyası duruma göre düzenlendi.

--- setup.py
+++ setup.py
@@ -6,22 +6,25 @@ import os
 
 os.system('pyrcc5 limestorelib.qrc -o limestorelib/resource.py')
 
-datas = [('/usr/share/applications', ['lime-store.desktop','lime-upmanager.desktop']),
-        ('/etc/skel/.config/autostart', ['lime-upmanager.desktop']),
-        ('/usr/share/pixmaps/', ['apps/lime-store.png','apps/lime-update-manager.png']),
-        ('/usr/share/Lime-Store/diller',glob.glob("diller/*.ts")),
-        ('/usr/share/Lime-Store/apps',glob.glob("apps/*.svg"))]
+datas = [
+    ('/usr/share/applications', ['limelinux-store.desktop','limelinux-updater.desktop']),
+    ('/etc/skel/.config/autostart', ['limelinux-updater.desktop']),
+    ('/usr/share/icons/hicolor/scalable/apps', ['data/limelinux-store.png','data/lime-update-manager.png']),
+    ('/usr/share/icons/hicolor/scalable/mimetypes', ["data/application-x-pisi.svg"],
+    ('/usr/share/limelinux-store/languages',glob.glob("languages/*.qm")),
+    ('/usr/share/limelinux-store/apps', glob.glob("apps/*.svg"))
+]
 
 setup(
-    name = "LimeLinux-Store",
-    scripts = ["lime-store","lime-upmanager"],
+    name = "limelinux-store",
+    scripts = ["limelinux-store","limelinux-updater"],
     packages = find_packages(),
-    version = "0.0",
+    version = "1.0",
     license = "GPL v3",
-    description = "LimeLinux Store And Update Manager",
+    description = "Lime GNU/Linux Store And Updater",
     author = "Fatih KAYA",
     author_email = "sonakinci41@gmail.com",
-    url = "https://github.com/sonakinci41/LimeLinux-Store",
+    url = "https://github.com/limelinux/LimeLinux-Store",
     keywords = ["PyQt5"],
     data_files = datas
 )
